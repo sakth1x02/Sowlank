6602b1e9f0bf7ac099f870214c7fbb4640b514a2
sakth1x02 <sakthishasumathi2002@gmail.com>
2025-03-04 11:24:39 +0530

    1

--- Sowalnk/vite.config.js
+++ Sowalnk/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:2000",
+        target: "http://54.211.117.56:2000",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
